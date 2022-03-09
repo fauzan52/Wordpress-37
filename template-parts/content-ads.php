9650709fd493a9b001a7e2a0e3a3d2dc281db441
fauzan52 <fauzan.alghifari17095@student.unsika.ac.id>
2022-03-09 08:22:00 +0700

    WP 37 - 09.03.2022 08.00

---
+++ template-parts/content-ads.php
@@ -0,0 +1,15 @@
+<div class="flex">
+    <div class="app-ads">
+        <div class="app-ads-card">
+            <div class="app-ads-card__title">
+                <h1>iklan disini bosku</h1>
+                <hr>
+            </div>
+            <div class="app-ads-card__text">
+                <h3>
+                    <a href="https://github.com/fauzan52">FAUZAN ALGHIFARI</a>
+                </h3>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ template-parts/content-first.php
@@ -0,0 +1,9 @@
+<div class="flex">
+    <div class="app-first">
+        <div class="app-first-card">
+            <div class="app-first-card__title">
+                <h1>iklan disini bosku</h1>
+            </div>
+        </div>
+    </div>
+</div>
